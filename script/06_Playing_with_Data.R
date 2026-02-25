64cf244cae129fac305b23d4cdc81004614b1441
nathaliegogniokane <nathaliegogniokane@gmail.com>
2026-02-25 12:58:47 +0100

    Growing season length indice creation and visualisation

--- script/06_Playing_with_Data.R
+++ script/06_Playing_with_Data.R
@@ -169,15 +169,95 @@ ggplot(df %>%
 # 1- We compute GSL : the growing season length (n°5)
 
 GSL_daily <- daily_temp_df %>%
-  filter(x == 1.75, y == 51) %>%
-  select(year, month, day, temp_C) 
+  filter(x == 6.75, y == 45.25)
+
+
+calc_gsl_fast <- function(temp, doy) {
+  # Let's make sure the data is in the right order
+  # (Even though is shoud already be thanks to the doy)
+  ord <- order(doy)
+  t <- temp[ord]
+  d <- doy[ord]
+  
+  # --- FIND THE START ---
+  # We are looking for 6 consecutive days > 5°C
+  runs_start <- rle(t > 5)
+  start_indices <- cumsum(runs_start$lengths) - runs_start$lengths + 1
+  
+  start_doy <- NA
+  for(i in seq_along(runs_start$values)) {
+    if(runs_start$values[i] == TRUE && runs_start$lengths[i] >= 6) {
+      start_doy <- d[start_indices[i]]
+      break
+    }
+  }
+  
+  # If there is no growing season starting, we return 0
+  if(is.na(start_doy)) return(0)
+  
+  # --- FIND THE END ---
+  # We can oly look at temperatures after the 1st of July (doy 182)
+  post_july_mask <- d >= 182
+  t_post <- t[post_july_mask]
+  d_post <- d[post_july_mask]
+  
+  end_doy <- max(d) # Default value if winter doesn't come back before 31/12
+  
+  runs_end <- rle(t_post < 5)
+  end_indices <- cumsum(runs_end$lengths) - runs_end$lengths + 1
+  
+  for(i in seq_along(runs_end$values)) {
+    if(runs_end$values[i] == TRUE && runs_end$lengths[i] >= 6) {
+      end_doy <- d_post[end_indices[i]]
+      break
+    }
+  }
+  
+  return(end_doy - start_doy)
+}
+
+# GSL by location and year
+gsl_results <- daily_temp_df %>%
+  group_by(x, y, year) %>%
+  summarize(gsl_days = calc_gsl_fast(temp_C, doy), .groups = "drop")
 
 
 # Il existe aussi un package : ClimInd et une fonction gsl 
 # pour faire ça mais j'arrive pas encore à le faire marcher
+# library(ClimInd)
 
-GSL_daily_noleap <- GSL_daily %>%
-  filter(!(month == 2 & day == 29))
+# Time to visualize the results !
 
 
-as.Date("09/28/2008", format = "%m / %d / %Y")
+ggplot(gsl_results %>% filter(year %in% c(2015, 2023)),
+       aes(x = x, y = y, color = gsl_days, fill = gsl_days)) +
+  geom_tile() + 
+  scale_fill_viridis_c(option = "turbo") +
+  scale_color_viridis_c(option = "turbo") +
+  facet_wrap(~ year) +  
+  theme_void() +
+  labs(title = "Comparison of Growing season length",
+       subtitle = "Years: 2015 vs 2023",
+       fill = "Growing season days",
+       color = "Growing season days")
+
+gsl_diff <- gsl_results %>% # To compute the difference between 2023/2015
+  filter(year %in% c(2015, 2023)) %>%
+  pivot_wider(names_from = year,
+              values_from = gsl_days,
+              names_prefix = "gsl_") %>%
+  mutate(dif = gsl_2023 - gsl_2015)
+
+ggplot(gsl_diff,
+       aes(x = x, y = y, color = dif, fill = dif)) +
+  geom_tile() + 
+  scale_fill_viridis_c(option = "turbo") +
+  scale_color_viridis_c(option = "turbo") +
+  theme_void() +
+  labs(title = "Evolution of Growing season length",
+       subtitle = "Years: 2015 to 2023",
+       fill = "Growing season difference",
+       color = "Growing season difference")
+
+summary(gsl_diff$dif) # Le représenter dans un joli tableau
+
